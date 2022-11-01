9ab76931b79e73126cd016ba0a93a663678e9691
Albert <alberto.suarez8@outlook.com>
2022-11-01 18:33:40 -0400

    Refactored repeated code.

--- assets/script.js
+++ assets/script.js
@@ -28,9 +28,9 @@ setInterval(function () {
     currentHour = moment().hour();
     if (currentHour < 9) {
         $("#9am, #10am, #11am, #12pm, #1pm, #2pm, #3pm, #4pm, #5pm").each(function () {
-            $(this).addClass("future")
+            $(this).addClass("future");
         });
-    }
+    } 
     if (currentHour > 17) {
         $("#9am, #10am, #11am, #12pm, #1pm, #2pm, #3pm, #4pm, #5pm").each(function () {
             $(this).addClass("past")
@@ -117,139 +117,45 @@ function alertLocalSave() {
     }, 2000);
 };
 
-function nineAMSave() {
+function saveBtn(timeValue) {
     var currentDay = moment().format("MMM Do");
     var localStorageValue = localStorage.getItem(currentDay);
-    var nineAMValue = $("#9am").val();
+    var value = $("#" + timeValue).val();
     if (localStorageValue) {
         var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["9am"] = nineAMValue;
+        parsedValue[timeValue] = value;
         localStorage.setItem(currentDay, JSON.stringify(parsedValue));
     } else {
-        localStorage.setItem(currentDay, JSON.stringify({"9am": nineAMValue}));
+        localStorage.setItem(currentDay, JSON.stringify({timeValue: value}));
     }
     alertLocalSave();
-};
-
-function tenAMSave() {
-    var currentDay = moment().format("MMM Do");
-    var localStorageValue = localStorage.getItem(currentDay);
-    var tenAMValue = $("#10am").val();
-    if (localStorageValue) {
-        var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["10am"] = tenAMValue;
-        localStorage.setItem(currentDay, JSON.stringify(parsedValue));
-    } else {
-        localStorage.setItem(currentDay, JSON.stringify({"10am": tenAMValue}));
-    }
-    alertLocalSave();
-};
-
-function elevenAMSave() {
-    var currentDay = moment().format("MMM Do");
-    var localStorageValue = localStorage.getItem(currentDay);
-    var elevenAMValue = $("#11am").val();
-    if (localStorageValue) {
-        var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["11am"] = elevenAMValue;
-        localStorage.setItem(currentDay, JSON.stringify(parsedValue));
-    } else {
-        localStorage.setItem(currentDay, JSON.stringify({"11am": elevenAMValue}));
-    }
-    alertLocalSave();
-};
-
-function twelvePMSave() {
-    var currentDay = moment().format("MMM Do");
-    var localStorageValue = localStorage.getItem(currentDay);
-    var twelvePMValue = $("#12pm").val();
-    if (localStorageValue) {
-        var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["12pm"] = twelvePMValue;
-        localStorage.setItem(currentDay, JSON.stringify(parsedValue));
-    } else {
-        localStorage.setItem(currentDay, JSON.stringify({"12pm": twelvePMValue}));
-    }
-    alertLocalSave();
-};
-
-function onePMSave() {
-    var currentDay = moment().format("MMM Do");
-    var localStorageValue = localStorage.getItem(currentDay);
-    var onePMValue = $("#1pm").val();
-    if (localStorageValue) {
-        var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["1pm"] = onePMValue;
-        localStorage.setItem(currentDay, JSON.stringify(parsedValue));
-    } else {
-        localStorage.setItem(currentDay, JSON.stringify({"1pm": onePMValue}));
-    }
-    alertLocalSave();
-};
-
-function twoPMSave() {
-    var currentDay = moment().format("MMM Do");
-    var localStorageValue = localStorage.getItem(currentDay);
-    var twoPMValue = $("#2pm").val();
-    if (localStorageValue) {
-        var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["2pm"] = twoPMValue;
-        localStorage.setItem(currentDay, JSON.stringify(parsedValue));
-    } else {
-        localStorage.setItem(currentDay, JSON.stringify({"2pm": twoPMValue}));
-    }
-    alertLocalSave();
-};
-
-function threePMSave() {
-    var currentDay = moment().format("MMM Do");
-    var localStorageValue = localStorage.getItem(currentDay);
-    var threePMValue = $("#3pm").val();
-    if (localStorageValue) {
-        var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["3pm"] = threePMValue;
-        localStorage.setItem(currentDay, JSON.stringify(parsedValue));
-    } else {
-        localStorage.setItem(currentDay, JSON.stringify({"3pm": threePMValue}));
-    }
-    alertLocalSave();
-};
-
-function fourPMSave() {
-    var currentDay = moment().format("MMM Do");
-    var localStorageValue = localStorage.getItem(currentDay);
-    var fourPMValue = $("#4pm").val();
-    if (localStorageValue) {
-        var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["4pm"] = fourPMValue;
-        localStorage.setItem(currentDay, JSON.stringify(parsedValue));
-    } else {
-        localStorage.setItem(currentDay, JSON.stringify({"4pm": fourPMValue}));
-    }
-    alertLocalSave();
-};
-
-function fivePMSave() {
-    var currentDay = moment().format("MMM Do");
-    var localStorageValue = localStorage.getItem(currentDay);
-    var nineAMValue = $("#5pm").val();
-    if (localStorageValue) {
-        var parsedValue = JSON.parse(localStorageValue);
-        parsedValue["5pm"] = nineAMValue;
-        localStorage.setItem(currentDay, JSON.stringify(parsedValue));
-    } else {
-        localStorage.setItem(currentDay, JSON.stringify({"5pm": fivePMValue}));
-    }
-    alertLocalSave();
-};
+}
 
 checkLocalStorage();
-nineAMSaveBtn.click(nineAMSave);
-tenAMSaveBtn.click(tenAMSave);
-elevenAMSaveBtn.click(elevenAMSave);
-twelvePMSaveBtn.click(twelvePMSave);
-onePMSaveBtn.click(onePMSave);
-twoPMSaveBtn.click(twoPMSave);
-threePMSaveBtn.click(threePMSave);
-fourPMSaveBtn.click(fourPMSave);
-fivePMSaveBtn.click(fivePMSave);
+nineAMSaveBtn.click(function() {
+    saveBtn("9am");
+});
+tenAMSaveBtn.click(function() {
+    saveBtn("10am");
+});
+elevenAMSaveBtn.click(function() {
+    saveBtn("11am");
+});
+twelvePMSaveBtn.click(function() {
+    saveBtn("12pm");
+});
+onePMSaveBtn.click(function() {
+    saveBtn("1pm");
+});
+twoPMSaveBtn.click(function() {
+    saveBtn("2pm");
+});
+threePMSaveBtn.click(function() {
+    saveBtn("3pm");
+});
+fourPMSaveBtn.click(function() {
+    saveBtn("4pm");
+});
+fivePMSaveBtn.click(function() {
+    saveBtn("5pm");
+});
